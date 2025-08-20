09e44d6ac6a5e446e10bb92a60df53487354404a
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-20 12:32:18 +0100

    Add load more button

--- src/components/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function LoadMoreBtn() {
-  return <div>LoadMoreBtn</div>;
+  return (
+    <div className="load-more-container">
+      <button className="load-more-btn">Load More</button>
+    </div>
+  );
 }
 
 export default LoadMoreBtn;
